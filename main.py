1694c5425ac820237c836ca32eaecdbdee2fd8f4
jdb-codes <jdbrown.tech@gmail.com>
2025-05-11 20:51:17 -0700

    added function to count letters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_letter_count
 
 def get_book_text(book):
     with open(book) as f:
@@ -6,6 +7,8 @@ def get_book_text(book):
 
 def main():
     num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    letter_count = get_letter_count(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    print(letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,19 @@
+import string
+
 def get_num_words(text):
     return len(text.split())
+
+def get_letter_count(text):
+    letter_count = {}
+    text = text.lower()
+    for character in text:
+        if character in string.ascii_lowercase:
+            if character in letter_count:
+                letter_count[character] += 1
+            else:
+                letter_count[character] = 1
+    return letter_count
+
+""" def get_sorted_dict(dict):
+    dict.sort(reverse=True)
+    print(dict) """
