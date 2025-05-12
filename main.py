5892d4b84cdac8274d0fd5e474ee51f3b2b5c178
jdb-codes <jdbrown.tech@gmail.com>
2025-05-11 21:20:47 -0700

    added sorted list, print report, command line arguments

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from stats import get_num_words
 from stats import get_letter_count
+from stats import get_sorted_dict
+import sys
 
 def get_book_text(book):
     with open(book) as f:
         return f.read()
 
+def print_report(book_location, word_count, letter_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_location}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dict in letter_count:
+        print(f"{dict["name"]}: {dict["num"]}")
+    print("============= END ===============")
+
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    letter_count = get_letter_count(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    print(letter_count)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_location = sys.argv[1]
+    num_words = get_num_words(get_book_text(book_location))
+    letter_count = get_letter_count(get_book_text(book_location))
+    letter_count_sorted = get_sorted_dict(letter_count)
+    print_report(book_location, num_words, letter_count_sorted)
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,3 @@
-import string
-
 def get_num_words(text):
     return len(text.split())
 
@@ -7,13 +5,21 @@ def get_letter_count(text):
     letter_count = {}
     text = text.lower()
     for character in text:
-        if character in string.ascii_lowercase:
+        if character.isalpha():
             if character in letter_count:
                 letter_count[character] += 1
             else:
                 letter_count[character] = 1
     return letter_count
 
-""" def get_sorted_dict(dict):
-    dict.sort(reverse=True)
-    print(dict) """
+def get_sorted_dict(dict):
+    letter_list = []
+    for key in dict:
+        letter_list.append({
+            "name": key,
+            "num": dict[key]
+        })
+    def sort_on(dict):
+        return dict["num"]
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
