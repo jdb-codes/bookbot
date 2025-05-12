e35955407611d1632c06ee4e8946844f64bf4fb1
jdb-codes <jdbrown.tech@gmail.com>
2025-05-11 20:17:44 -0700

    refactored to split functions to stats.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from stats import get_num_words
 
 def get_book_text(book):
     with open(book) as f:
         return f.read()
 
-def count_words(text):
-    return len(text.split())
-
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
 
 main()
