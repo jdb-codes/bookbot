6e40fcdcad1970063debb2c3657f455b1cb178c3
jdb-codes <jdbrown.tech@gmail.com>
2025-05-11 20:13:07 -0700

    changed main to print # of words in doc

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def get_book_text(book):
     with open(book) as f:
         return f.read()
 
+def count_words(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 
 main()
